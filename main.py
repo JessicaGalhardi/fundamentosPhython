67af5fbd0e55df79c60ab71e41c79dc5848a828c
JessicaGalhardi <jessica.galhardi@al.infnet.edu.br>
2023-04-27 14:00:10 +0000

    Function subtraction commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from operators.operators import add
+from operators.operators import sub
 
 print(add(4.3,6))
+
+print(sub(10,5))
+
+#Using a function with another function
+
+print(round(add(100, sub(41.777,3.7)),2))
--- operators/operators.py
+++ operators/operators.py
@@ -8,3 +8,14 @@ def add(a,b):
       return: int, float
   """
   return a + b
+
+def sub(a,b):
+  """ It subtract two numbers
+
+      param a: First value
+      type a: int,float
+      param b: Second value
+      type b: int,float
+      return: int, float
+  """
+  return a - b
